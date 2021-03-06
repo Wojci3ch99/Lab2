20b49cfaf56ae8c7abd143408cffa23dcf2ebf9b
Wojci3ch99 <Wojciech9905@gmail.com>
2021-03-06 21:44:23 +0100

    Commit pierwszy

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,15 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Account acc = new Account();
+        acc.setName("Piotr Gołabek");
+        System.out.println(acc.getName());
+    }
+
+
+}
+
